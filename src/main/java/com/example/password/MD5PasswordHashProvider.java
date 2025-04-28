2afebbe6e2f30dc06667e0fe163821190a79e400
anonym-collaborator <83634858+anonym-collaborator@users.noreply.github.com>
2025-04-28 09:22:25 +0300

    Update MD5PasswordHashProvider.java

--- src/main/java/com/example/password/MD5PasswordHashProvider.java
+++ src/main/java/com/example/password/MD5PasswordHashProvider.java
@@ -15,4 +15,45 @@ public class MD5PasswordHashProvider implements PasswordHashProvider {
 	public MD5PasswordHashProvider(String providerId) {
 		this.providerId = providerId;
 	}
+
+	@Override
+	public void close() {
+	}
+
+	@Override
+	public boolean policyCheck(PasswordPolicy policy, PasswordCredentialModel credential) {
+		return this.providerId.equals(credential.getPasswordCredentialData().getAlgorithm());
+	}
+
+	@Override
+	public PasswordCredentialModel encodedCredential(String rawPassword, int iterations) {
+		String encodedPassword = this.encode(rawPassword, iterations);
+		return PasswordCredentialModel.createFromValues(this.providerId, new byte[0], iterations, encodedPassword);
+	}
+
+	@Override
+	public boolean verify(String rawPassword, PasswordCredentialModel credential) {
+		String encodedPassword = this.encode(rawPassword, credential.getPasswordCredentialData().getHashIterations());
+		String hash = credential.getPasswordSecretData().getValue();
+		return encodedPassword.equals(hash);
+	}
+
+	@Override
+	public String encode(String rawPassword, int iterations) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(password.getBytes(StandardCharsets.US_ASCII));
+            return bytesToHex(digest).toLowerCase();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 Algorithm not found", e);
+        }
+	}
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
 }
